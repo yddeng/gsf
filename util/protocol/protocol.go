8feddca0211d45ad6182afe9922680f9c2da53f9
yidongdeng <248244142@qq.com>
2020-08-26 14:43:05 +0800

    queue

--- util/protocol/protocol.go
+++ util/protocol/protocol.go
@@ -19,44 +19,33 @@ type Protocol struct {
 	protoc  Protoc
 }
 
-var pProtocol *Protocol
-
-func InitProtocol(protoc Protoc) {
-	pProtocol = &Protocol{
+func NewProtoc(protoc Protoc) *Protocol {
+	return &Protocol{
 		id2Type: map[uint16]reflect.Type{},
 		type2Id: map[reflect.Type]uint16{},
 		protoc:  protoc,
 	}
 }
 
-func Register(id uint16, msg interface{}) {
-	if pProtocol == nil {
-		fmt.Errorf("protocol is nil,need init")
-		return
-	}
-
+func (this *Protocol) Register(id uint16, msg interface{}) error {
 	tt := reflect.TypeOf(msg)
 
-	if _, ok := pProtocol.id2Type[id]; ok {
-		fmt.Errorf("%d already register to type:%s", id, tt)
-		return
+	if _, ok := this.id2Type[id]; ok {
+		return fmt.Errorf("%d already register to type:%s", id, tt)
 	}
 
-	pProtocol.id2Type[id] = tt
-	pProtocol.type2Id[tt] = id
+	this.id2Type[id] = tt
+	this.type2Id[tt] = id
+	return nil
 }
 
-func Marshal(data interface{}) (uint16, []byte, error) {
-	if pProtocol == nil {
-		return 0, nil, fmt.Errorf("protocol is nil,need init")
-	}
-
-	id, ok := pProtocol.type2Id[reflect.TypeOf(data)]
+func (this *Protocol) Marshal(data interface{}) (uint16, []byte, error) {
+	id, ok := this.type2Id[reflect.TypeOf(data)]
 	if !ok {
 		return 0, nil, fmt.Errorf("type: %s undefined", reflect.TypeOf(data))
 	}
 
-	ret, err := pProtocol.protoc.Marshaler(data)
+	ret, err := this.protoc.Marshaler(data)
 	if err != nil {
 		return 0, nil, err
 	}
@@ -64,12 +53,8 @@ func Marshal(data interface{}) (uint16, []byte, error) {
 	return id, ret, nil
 }
 
-func Unmarshal(msgID uint16, data []byte) (msg interface{}, err error) {
-	if pProtocol == nil {
-		return nil, fmt.Errorf("protocol is nil,need init")
-	}
-
-	tt, ok := pProtocol.id2Type[msgID]
+func (this *Protocol) Unmarshal(msgID uint16, data []byte) (msg interface{}, err error) {
+	tt, ok := this.id2Type[msgID]
 	if !ok {
 		err = fmt.Errorf("msgID: %d undefined", msgID)
 		return
@@ -77,7 +62,7 @@ func Unmarshal(msgID uint16, data []byte) (msg interface{}, err error) {
 
 	//反序列化的结构
 	msg = reflect.New(tt.Elem()).Interface()
-	err = pProtocol.protoc.Unmarshaler(data, msg)
+	err = this.protoc.Unmarshaler(data, msg)
 	if err != nil {
 		return nil, err
 	}
